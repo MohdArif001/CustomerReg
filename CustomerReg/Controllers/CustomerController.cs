2769f42d4d3b665572dc079ec57b5b50d4530c56
ariffuzu001@gmail.com <ariffuzu001@gmail.com>
2022-04-13 12:53:21 +0530

    Add project files.

---
+++ CustomerReg/Controllers/CustomerController.cs
@@ -0,0 +1,54 @@
+﻿using CustomerReg.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CustomerReg.Controllers
+{
+    public class CustomerController : Controller
+    {
+
+        public ApplicationDbContext dbContext { get; private set; }
+        public CustomerController(ApplicationDbContext context)
+        {
+            dbContext = context;
+        }
+
+        public IActionResult ShowAllCus()
+        {
+            var customers = dbContext.Customers.ToList();
+            return View(customers);
+        }
+
+        public IActionResult CreateCustomer()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult CreateCustomer(Customer customer)
+        {
+            dbContext.Add(customer);
+            dbContext.SaveChanges();
+            
+            return RedirectToAction("ShowAllCus");
+        }
+        public JsonResult GetCountries()
+        {
+            var countries = dbContext.Countries.ToList();
+            return Json(countries);
+        }
+
+        public JsonResult GetStates(int id)
+        {
+            var states = dbContext.States.Where(e => e.CountId == id).ToList();
+            return Json(states);
+        }
+        public JsonResult GetCities(int id)
+        {
+            var cities = dbContext.Cities.Where(e => e.StateId == id).ToList();
+            return Json(cities);
+        }
+    }
+}
---
+++ CustomerReg/Models/Customer.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CustomerReg.Models
+{
+    public class Customer
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Gender { get; set; }
+        public Country Country { get; set; }
+        public State State { get; set; }
+        public City City { get; set; }
+
+    }
+}
